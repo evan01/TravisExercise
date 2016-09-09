8e2d3041907016a0efecc6038ed45d35d3d4beb5
eknox <eknoxmobile@gmail.com>
2016-09-09 15:06:30 -0400

    tests!

---
+++ test/strategydemo/FlatRateDiscountStrategyTest.java
@@ -0,0 +1,17 @@
+package strategydemo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * TravisExercise
+ * Created by eknox on 2016-09-09.
+ */
+public class FlatRateDiscountStrategyTest {
+    @Test
+    public void applyDiscount() throws Exception {
+
+    }
+
+}
